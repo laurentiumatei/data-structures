b61a1f39c27346727bffa097f02ab17820ea24fc
laurentiumatei <laurentiumatei11@gmail.com>
2017-10-17 14:55:56 +0300

    First commit
    Change-Id: I5bde8d3dea79b5bf1eba643372d57901296c54b3

---
+++ src/main/java/com/example/stack/BasicStack.java
@@ -0,0 +1,48 @@
+package com.example.stack;
+
+public class BasicStack<X> {
+	private X [] data;
+	private int stackPointer;
+
+	public BasicStack() {
+		data = (X[]) new Object[1000];
+		stackPointer = 0;
+	}
+	
+	public void push(X newItem) {
+		data[stackPointer++] = newItem;
+	}
+	
+	public X pop() {
+		if (stackPointer == 0) {
+			throw new IllegalStateException("No more items on the stack");
+		}
+
+		return data[--stackPointer];
+	}
+	
+	public boolean contains(X item) {
+		for (int i = 0; i< stackPointer; i++) {
+			if (data[i].equals(item)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public X access(X item) {
+		while (stackPointer > 0) {
+			X tempItem = pop();
+			if (item.equals(tempItem)) {
+				return tempItem;
+			}
+		}
+		
+		throw new IllegalArgumentException("Coulnd not find item on the stack: "+item);
+	}
+	
+	public int size() {
+		return stackPointer;
+	}	
+}
